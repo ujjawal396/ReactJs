9e6631c0d02ae5022e7217ae3729c9d72feac792
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-10 20:08:19 +0530

    basic react over

---
+++ Lecture1/app.js
@@ -0,0 +1,7 @@
+ const ele=React.createElement("h1",
+     
+    {id:"heading"},
+    
+    "Hello world from react!");
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(ele);
