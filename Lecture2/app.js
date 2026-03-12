541eaf41a9ef9cc6f175db3880b66adc7a1fa704
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-12 19:13:39 +0530

    functional component rendering and component composition

--- Lecture2/app.js
+++ Lecture2/app.js
@@ -4,15 +4,25 @@ import { jsxs } from "react/jsx-runtime";
  
 //JSX(TRANSPILED before it reaches js)-parcel-babel
 //Babel=>transpiles into react.createelement=>js object=>HTMLelement(render)
- const jsx=<h1>Hello from JSX !</h1>
+ const TitleElement=()=>{
+   return (<h1>Namaste React using jsx</h1>)
+ }
 
 //everything in react is component
 //class based component
 //functinal component
 
-
+//component composition
 const HeadingElement=()=>{
-   return (<h1>Rahul is my friend</h1>)
+   return (
+      <div>
+      <TitleElement/>
+         <h1>Rahul is my friend</h1>
+      </div>
+   
+   
+
+)
 }
 
 const headingElement2=()=> (<h1>rahul is my friend</h1>)
@@ -24,4 +34,4 @@ const headingElement2=()=> (<h1>rahul is my friend</h1>)
 
 
     const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(jsx);
+    root.render(<HeadingElement/>);
