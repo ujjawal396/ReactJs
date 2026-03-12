0434c7b1ce99f6b7453ae8a6277a8697fb08151b
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-12 19:10:13 +0530

    how jsx gets rendered

--- Lecture2/app.js
+++ Lecture2/app.js
@@ -1,11 +1,27 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
+import { jsxs } from "react/jsx-runtime";
  
- 
- const ele=React.createElement("h1",
-     
-    {id:"heading"},
-    
-    "Hello world from react!");
+//JSX(TRANSPILED before it reaches js)-parcel-babel
+//Babel=>transpiles into react.createelement=>js object=>HTMLelement(render)
+ const jsx=<h1>Hello from JSX !</h1>
+
+//everything in react is component
+//class based component
+//functinal component
+
+
+const HeadingElement=()=>{
+   return (<h1>Rahul is my friend</h1>)
+}
+
+const headingElement2=()=> (<h1>rahul is my friend</h1>)
+
+
+
+
+
+
+
     const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(ele);
+    root.render(jsx);
