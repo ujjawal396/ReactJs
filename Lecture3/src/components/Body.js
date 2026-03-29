14a8f08857ca3caa4801511e5883d43ff5440532
ujjawal396 <ujjawaljha08@gmail.com>
2026-03-30 00:41:53 +0530

    live api working

--- Lecture3/src/components/Body.js
+++ Lecture3/src/components/Body.js
@@ -1,33 +1,70 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+   const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  return (
-    <div className="body">
-      <div className="filter">
+  const [searchText, setSearchText] = useState("");
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  console.log("Body Rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    //console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+    // Optional Chaining
+    setListOfRestraunt(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+return listOfRestaurants.length === 0 ? (
+  <Shimmer />
+) : (
+  <div className="body">
+    <div className="filter">
+      <div className="search">
+        <input
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+
         <button
-          className="filter-btn"
           onClick={() => {
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.data.avgRating > 4
+            const filtered = listOfRestaurants.filter((res) =>
+              res.info.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase())
             );
-            setListOfRestraunt(filteredList);
+            setFilteredRestaurant(filtered);
           }}
         >
-          Top Rated Restaurants
+          Search
         </button>
       </div>
-      <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-        ))}
-      </div>
     </div>
-  );
-};
+
+    <div className="res-container">
+      {filteredRestaurant.map((restaurant) => (
+        <RestaurantCard
+          key={restaurant.info.id}
+          resData={restaurant}
+        />
+      ))}
+    </div>
+  </div>
+);
+}
 
 export default Body;
--- Lecture3/src/components/RestaurantCard.js
+++ Lecture3/src/components/RestaurantCard.js
@@ -9,8 +9,10 @@ const RestaurantCard = (props) => {
     avgRating,
     cuisines,
     costForTwo,
-    deliveryTime,
-  } = resData?.data;
+       sla,
+  } = resData?.info;
+
+  
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -18,12 +20,15 @@ const RestaurantCard = (props) => {
         className="res-logo"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
+
+       
       />
+       {console.log(CDN_URL+cloudinaryImageId)}
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>₹{costForTwo}</h4>
+      <h4>{sla?.slaString} minutes</h4>
     </div>
   );
 };
